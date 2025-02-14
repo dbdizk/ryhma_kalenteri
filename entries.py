0a4852ab7ca77f568190aa1ce077afb8dff09ca4
Nikku Onni J <onikku@cubbli24-gold.cs.helsinki.fi>
2025-02-14 10:46:55 +0200

    Made it possible to examine singular events in detail

--- entries.py
+++ entries.py
@@ -8,3 +8,7 @@ def add_entry(title,description,date,duration,user_id):
 def get_entries():
     sql = "SELECT id, title FROM entries ORDER BY id DESC"
     return db.query(sql)
+
+def get_entry(entry_id):
+    sql = """SELECT entries.title, entries.description, entries.date, entries.duration, users.username FROM entries, users WHERE entries.user_id = users.id AND entries.id = ?"""
+    return db.query(sql, [entry_id])[0]
